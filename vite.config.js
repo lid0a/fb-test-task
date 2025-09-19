21b0a6d6d7ec506cb50a087cddfab8314be6c5af
Odil Abdulloyev <abdulloyevodil@gmail.com>
2025-09-19 18:56:10 +0500

    chore: setup vie-plugin-singlefile

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import path from 'node:path';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
   resolve: {
@@ -7,4 +8,5 @@ export default defineConfig({
       '~': path.resolve(import.meta.dirname, 'src'),
     },
   },
+  plugins: [viteSingleFile()],
 });
